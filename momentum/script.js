f5091e24fcfc82f032a91bf22542b3d5d38e31a0
Alex Avtushko <pseledkin@yandex.by>
2020-10-23 02:33:53 +0300

    feat: add clear of input

--- momentum/script.js
+++ momentum/script.js
@@ -1,9 +1,9 @@
 // DOM Elements
-const time = document.querySelector('.time'),
-date = document.querySelector('.date'),
-  greeting = document.querySelector('.greeting'),
-  name = document.querySelector('.name'),
-  focus = document.querySelector('.focus');
+const time = document.querySelector(".time"),
+  date = document.querySelector(".date"),
+  greeting = document.querySelector(".greeting"),
+  name = document.querySelector(".name"),
+  focus = document.querySelector(".focus");
 
 // Options
 const showAmPm = true;
@@ -16,100 +16,118 @@ function showTime() {
     sec = today.getSeconds();
 
   // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
+  const amPm = hour >= 12 ? "PM" : "AM";
 
   // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )}`;
 
   setTimeout(showTime, 1000);
 }
 
 function showDate() {
   let today = new Date();
-    let options = { weekday: 'long', month: 'long', day: 'numeric' };
+  let options = { weekday: "long", month: "long", day: "numeric" };
   // Output Date
-  date.innerHTML = `${new Intl.DateTimeFormat('en-US', options).format(today)}`;
+  date.innerHTML = `${new Intl.DateTimeFormat("en-US", options).format(today)}`;
 
   setTimeout(showDate, 1000);
 }
 // Add Zeros
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
 // Set Background and Greeting
 function setBgGreet() {
   let today = new Date(),
     hour = today.getHours();
+    const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 
   if (hour < 12) {
     // Morning
     document.body.style.backgroundImage =
       "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning, ';
+    greeting.textContent = "Good Morning, ";
   } else if (hour < 18) {
     // Afternoon
     document.body.style.backgroundImage =
       "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
+    greeting.textContent = "Good Afternoon, ";
   } else {
     // Evening
     document.body.style.backgroundImage =
       "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening, ';
-    document.body.style.color = 'white';
+    greeting.textContent = "Good Evening, ";
+    document.body.style.color = "white";
   }
 }
 
 // Get Name
 function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
   } else {
-    name.textContent = localStorage.getItem('name');
+    name.textContent = localStorage.getItem("name");
   }
 }
 
 // Set Name
 function setName(e) {
-  if (e.type === 'keypress') {
+  if (e.type === "keypress") {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
-      name.blur();
+      if (e.target.innerText !== "") {
+        localStorage.setItem("name", e.target.innerText);
+        name.blur();
+      } else getName();
     }
   } else {
-    localStorage.setItem('name', e.target.innerText);
+    if (e.target.innerText !== "") {
+      localStorage.setItem("name", e.target.innerText);
+    } else getName();
   }
+}
+
+function clearInput(e) {
+  e.target.innerText = "";
 }
 
 // Get Focus
 function getFocus() {
-  if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Focus]';
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
   } else {
-    focus.textContent = localStorage.getItem('focus');
+    focus.textContent = localStorage.getItem("focus");
   }
 }
 
 // Set Focus
 function setFocus(e) {
-  if (e.type === 'keypress') {
+  if (e.type === "keypress") {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('focus', e.target.innerText);
+      if (e.target.innerText !== "") {
+      localStorage.setItem("focus", e.target.innerText);
       focus.blur();
+      }
+      else getFocus();
     }
   } else {
-    localStorage.setItem('focus', e.target.innerText);
+    if (e.target.innerText !== "") {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+  else  getFocus();
   }
 }
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
-
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+name.addEventListener("click", clearInput);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+focus.addEventListener("click", clearInput);
 // Run
 showDate();
 showTime();
